0787891648e8112c9351454619254cf83135365f
Aaron Rojas <56888910+aaronrojas32@users.noreply.github.com>
2023-03-10 18:16:27 +0100

    Update power_plan.bat

--- power_plan.bat
+++ power_plan.bat
@@ -5,9 +5,9 @@ for /f "tokens=2 delims==." %%a in ('wmic path Win32_Battery Get BatteryStatus /
 for /f "tokens=2 delims==." %%b in ('wmic path Win32_Battery Get EstimatedChargeRemaining /value') do set charge=%%b
 
 if %status% equ 2 (
-    powercfg -SETACTIVE 6f6c12a8-f34d-4d4a-ba70-5af731d6177b
+    powercfg -SETACTIVE GUID_ENCHUFADO
 ) else (
-    powercfg -SETACTIVE 381b4222-f694-41f0-9685-ff5bb260df2e
+    powercfg -SETACTIVE GUID_BATERIA
 )
 
 timeout /t 5 >nul
